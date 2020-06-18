8813f0e3ea2a34da0ff15129da0eb08dc357b336
Cypres11 <37955322+Cypres11@users.noreply.github.com>
2020-06-18 08:02:06 +0200

    Add files via upload
    The first try to create a repository for a future website on the 5 important sources to have a good life

---
+++ hello_world.py
@@ -0,0 +1,13 @@
+#crash course programming in python by
+#Eric Matthes, book issued by Visual Steps
+
+# pg 90 
+message='hello Python world!'
+print(message)
+message='hello Python Crash Course world!'
+print(message)
+message='hello Roland!!'
+print(message)
+#Roland uses this file for code learning
+
+
